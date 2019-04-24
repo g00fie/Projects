5d41de5f5f1d79202d794fc7682a71c172503b4e
maciejbilinski2 <thepigmansuper@gmail.com>
2019-04-25 00:47:09 +0200

    Dodanie skryptu node.js do RĘCZNEJ konwersji pliku .html do pliku .twig

---
+++ NODE/convert_html_to_twig.js
@@ -0,0 +1,32 @@
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+var fs = require('fs');
+
+fs.readFile('../public/starting-page.html', 'utf8', function(err, contents) {
+    const dom = new JSDOM(contents);
+    const document = dom.window.document;
+    
+    //zmiana ścieżki na bezwzględną
+    document.querySelectorAll("*[href^='css']").forEach(function(item){
+        item.setAttribute("href", "/" + item.getAttribute("href"));
+    });
+    document.querySelectorAll("*[src^='js'], *[src^='img']").forEach(function(item){
+        item.setAttribute("src", "/" + item.getAttribute("src"));
+    });
+    
+    //zmiana formularza rejestracji na twigową wersję
+    {
+        var form = document.getElementById("registerForm");
+        var parent = form.parentElement;
+        parent.removeChild(form);
+        parent.innerHTML += `
+            {% form_theme register 'layouts/form_register_layout.html.twig' %}
+            {{ form(register) }}
+        `;
+    }
+    
+    fs.writeFile("../templates/starting-page.html.twig", dom.serialize(), function(err){
+        console.log("done");
+    });
+});
